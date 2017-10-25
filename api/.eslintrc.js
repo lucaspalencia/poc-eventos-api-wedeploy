8e3557dc91e1e75989ac7d1dc6ebb81b7c8f15bc
Lucas Palencia <lucas.palencia@vizir.com.br>
2017-10-25 16:46:52 -0200

    [Style] .eslintrc

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -2,10 +2,10 @@ module.exports = {
   'extends': 'airbnb-base',
   'env': {
     'node': true,
-	},
+  },
   'rules': {
     'semi': ['error', 'never'],
     'no-console': 0,
     'comma-dangle': ['error', 'never']
-	},
+  }
 };
